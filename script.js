72e54227caf9db3eebdad1084caf60f27e3a0bee
kurtrode <112707023+kurtrode@users.noreply.github.com>
2023-05-05 11:12:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 //     newDiv.innerHTML = `<a href="${repoJSON[5].url}">${repoJSON[5].name}</a>*/`
     repoJSON.forEach(linker =>{
         body.appendChild(newDiv)
-        newDiv.innerHTML += `<a href='${linker.url}' style="color:red;text-decoration:none>${linker.name}</a><br/><br/>`
+        newDiv.innerHTML += `<a href='${linker.url}' style="color:red;text-decoration:none">${linker.name}</a><br/><br/>`
     })
  }
 fetchRepos()
